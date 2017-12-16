c5a96ba0624a675a448cf82e6a325525189e408b
felsiq <felipe.siqueira@usp.br>
2017-12-16 13:18:15 -0200

    Added Gram-Schmdit Orthonormalization algorithm.

---
+++ gramSchmidtOrthonormalization.py
@@ -0,0 +1,94 @@
+"""
+Expected input: n vectors coordinates, each one in a column, separated by whitespaces
+Example:
+1 1 5
+2 1 4
+3 3 9
+
+where v1 = [1 2 3]^T, v2 = [1 1 3]^T and v3 = [5 4 9]^T
+
+General case (m vectors with n dimensions:
+x1	y1	z1	..	k1
+x2	y2	z2	..	k2
+x3	y3	z3	..	k3
+..	..	..	..	..
+xn	yn	zn	..	kn
+
+where v1 = [x1 x2 x3 .. xn]^T, ..., vm = [k1 k2 k3 .. kn]^T
+
+"""
+
+with open('rref.py', 'r') as f:
+	exec(f.read())
+
+def checkIndenpendency(vecs):
+	vCopy = mcopy(vecs)
+	reducted, reductor = rref(vCopy)
+	
+	dep = 0
+	for r in reducted:
+		dep += sum(r) == 0
+	
+	return dep == 0
+
+def vecnorm(vec):
+	norm = 0
+	for i in vec:
+		norm += i[0]**2
+	return norm**0.5
+
+def gso(vecs):
+	# 1. Check indenpendency two-by-two
+	colVecs = mtransp(vecs)
+
+	if not checkIndenpendency(colVecs):
+		print('E: These vectors aren\'t indenpendent.')
+		return None
+
+	for i in range(len(colVecs)):
+		for j in range(len(vecs)):
+			colVecs[i][j] = [colVecs[i][j]]
+
+	newVectors = list()
+	# 2. Apply Gram process with Schmidt operation
+	for i in range(len(colVecs)):
+		newBaseVec = [[k[0]] for k in colVecs[i]]
+		for j in range(i):
+			coeff1 = mmult(mtransp(newVectors[j]), newBaseVec)
+			coeff2 = mmult(mtransp(newVectors[j]), newVectors[j])
+			curProj = mmconst(newVectors[j], coeff1/coeff2)
+			newBaseVec = msub(newBaseVec, curProj)
+
+		newVectors.append(mmconst(newBaseVec, 1.0/vecnorm(newBaseVec)))
+
+	return newVectors
+
+if __name__ == '__main__':
+	vecs = list()
+	flag = True
+	while flag:
+		try:
+			vecs.append(list(map(float, input().split(' '))))
+		except:
+			flag = False
+	newvecs = gso(vecs)
+
+	if newvecs:
+		letter = ord('a')
+		for v in newvecs:
+			print(chr(letter) + ':', v)
+			letter += 1
+
+		# Testing if they're orthogonal two by two
+		# test: if i == j then != 0, 0 otherwise
+		# In other words, this must print a diagonal matrix.
+		# Because the vectors should be unitary, then this diag.
+		# matrix must be the Identity Matrix.
+		print('\nTruth proof (should always be the identity matrix):')
+		nVecs = len(newvecs)
+		resultMat = minit(nVecs, nVecs)
+		for i in range(nVecs):
+			for j in range(nVecs):
+				resultMat[i][j] = mmult(mtransp(newvecs[i]), newvecs[j])
+
+		mprint(resultMat)
