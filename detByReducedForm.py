2d355a8bdb3257007ec947b914c8f9262be5b6d5
FelSiq <felipe.siqueira@usp.br>
2017-12-17 22:17:08 -0200

    Added a matrix determinant calculator that uses echelon form.

---
+++ detByReducedForm.py
@@ -0,0 +1,26 @@
+with open('rref.py', 'r') as f:
+	exec(f.read())
+
+def reducedDet(m):
+	if len(m) != len(m[0]):
+		print('Matrix should be square to have a properly determinant.')
+		return None
+
+	reduced, elim = rref(m, reduced = False)
+
+	det = 1.0
+	for i in range(len(reduced)):
+		det *= reduced[i][i]
+	return det
+
+if __name__ == '__main__':
+	mat = [
+		[1, 4, 5, 9, -29, 27],
+		[2, -1, 1, 1, 23, -1],
+		[3, 0, 7, -2, 17, 0],
+		[1, 2, 1, -1, 0, 0],
+		[-30, 20, -1, -1, 17, 1],
+		[9, 9, -1, -2, -3, 0],
+	]
+
+	print(reducedDet(mat))
