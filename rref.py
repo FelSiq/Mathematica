b017a8dc550107a4dfe53e8db4d4a76126dcb33e
FelSiq <felipe.siqueira@usp.br>
2017-12-17 22:16:36 -0200

    Now rref have the option to just compute echelon form.

--- rref.py
+++ rref.py
@@ -1,7 +1,7 @@
 with open('matrix.py', 'r') as f:
 	exec(f.read())
 
-def rref(m):
+def rref(m, reduced = True):
 	# if pivot, in module, is smaller than DELTA, assume it's zero
 	DELTA = 1.0e-10
 
@@ -30,7 +30,7 @@ def rref(m):
 			shift += 1
 		else:
 			# Just some row operations with the augmented matrix
-			for j in range(mRow):
+			for j in range(i * (not reduced), mRow):
 				if j != i:
 					multiplier = mCopy[j][i + shift]/pivot
 					for k in range(mCol):
@@ -38,23 +38,28 @@ def rref(m):
 					for k in range(mRow):
 						mAug[j][k] -= mAug[i][k] * multiplier
 			# Set the pivots to 1
-			for k in range(mCol):
-				mCopy[i][k] /= pivot
-			for k in range(mRow):
-				mAug[i][k] /= pivot
+			if reduced:
+				for k in range(mCol):
+					mCopy[i][k] /= pivot
+				for k in range(mRow):
+					mAug[i][k] /= pivot
 			i += 1
 
 	return mCopy, mAug
 
 # Program drive
-"""if __name__ == '__main__':
+"""
+if __name__ == '__main__':
 	mat = [
-		[1, 0, 2, 7],
-		[2, -1, -2, 7],
-		[3, 0, 6, 6],
+		[1, 4, 5],
+		[2, -1, 1],
+		[3, 0, 7],
 	]
 
-	reducted, elimination = rref(mat)
+
+	# If reduced = False, the the output should be the Upper Triangular Echelon Form Matrix.
+	# Otherwise, it will be the Row Reduced Echelon Form diagonal (if square) matrix.
+	reducted, elimination = rref(mat, reduced = True)
 	print('Matrix:')
 	mprint(mat)
 	print('\n\nReducted (should be identity for reversible square matrix):')
@@ -72,4 +77,5 @@ def rref(m):
 	mprint(redInverse)
 
 	print('\n\nElimination^-1 x Reducted == Matrix? :')
-	mprint(mmult(redInverse, reducted))"""
+	mprint(mmult(redInverse, reducted))
+"""
