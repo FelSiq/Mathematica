df372e81f71010bb7533fa68b570e5a500bddf52
FelSiq <felipe.siqueira@usp.br>
2018-03-31 09:26:29 -0300

    Improved Bissection Method interface

--- functionZeroByBissection.py
+++ functionZeroByBissection.py
@@ -1,16 +1,18 @@
 from rpy2 import robjects as ro
 import regex
+import sys
 
 def bissection(function, a, b, n=100, retList=False, var='x'):
 	start = a
 	end = b
 	xMeans = list()
+	subVarRe = regex.compile(r'\b' + var + r'\b')
 	for i in range(n):
 		curXMean = (start + end) * 0.5		
 		xMeans.append(curXMean)
 		
-		val1 = ro.r(regex.sub(var, str(curXMean), function))[0] 
-		val2 = ro.r(regex.sub(var, str(start), function))[0] 
+		val1 = ro.r(subVarRe.sub(str(curXMean), function))[0] 
+		val2 = ro.r(subVarRe.sub(str(start), function))[0] 
 
 		if val1 * val2 > 0:
 			# The sign does not change between f(curXMean) and f(start) i.e. the 
@@ -27,7 +29,26 @@ def bissection(function, a, b, n=100, retList=False, var='x'):
 	return xMeans.pop()
 
 if __name__ == '__main__':
-	function = '3*x^5 + 2*x^3 - 2*x^2 + 7*x + 4.5'	
-	root = bissection(function, -600, 600)
-	print('root (z):', root, '\tAbsError:', abs(ro.r(regex.sub('x', str(root), function))[0])) 
+	if len(sys.argv) < 4:
+		print('usage:', sys.argv[0], '<function> <a> <b> (interval is [a, b]) <# of iterations? (optional)>')
+		exit(1)
+	function = sys.argv[1]
+	try:
+		a = float(sys.argv[2])
+		b = float(sys.argv[3])
+	except:
+		print('Error: \'a\' and \'b\' args must be a real number.')
+		exit(2)
+	try:
+		n = 100
+		if len(sys.argv) == 5:
+			n = int(sys.argv[4])
+		if n <= 0:
+			raise Exception()
+	except:
+		print('Error: number of iterations must be a positive natural value.')
+		exit(3)
+
+	root = bissection(function, a, b, n)
+	print('root (z):', root, '\tAbsError:', abs(ro.r(regex.sub(r'\bx\b', str(root), function))[0])) 
 	
