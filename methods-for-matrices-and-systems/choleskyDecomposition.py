3fa48486d0716280201ca2bcc1efec842c4094b0
FelSiq <felipe.siqueira@usp.br>
2018-04-01 20:52:37 -0300

    Fixed a Cholesky Dec glitch.

--- methods-for-matrices-and-systems/choleskyDecomposition.py
+++ methods-for-matrices-and-systems/choleskyDecomposition.py
@@ -1,4 +1,5 @@
 from matrix import matrix, rref
+import sys
 
 """
 Input: Symmetric (A = A^T) and Positive Definite (x^T * A * x > 0 for all x in R^n) square matrix A[n x n].
@@ -31,7 +32,7 @@ def cholesky(M):
 	for k in range(n):
 		s = 0.0
 		for j in range(k):
-			s = L[k][j]**2.0
+			s += (L[k][j])**2.0
 
 		if M[k][k] >= s:
 			L[k][k] = (M[k][k] - s)**0.5
@@ -39,31 +40,18 @@ def cholesky(M):
 			print('Error: M matrix is not positive definite (can\'t perform Cholesky Decomposition)!')
 			return None
 
-		for j in range(k+1, n):
+		for i in range(k+1, n):
 			s = 0.0
-			for i in range(k):
-				s = L[j][k] * L[k][i]
-			L[j][k] = 1.0/L[k][k] * (M[j][k] - s) 
+			for j in range(k):
+				s += L[i][j] * L[k][j]
+			L[i][k] = (M[i][k] - s)/L[k][k]
 
 	return L
 
 if __name__ == '__main__':
-	M1 = [
-		[2, -1, 0],
-		[-1, 2, -1],
-		[0, -1, 2]
-	]
-	M2 = [
-		[6, 4, -3],
-		[4, -2, 0],
-		[-3, 0, 1]
-	]
-	M3 = [
-		[6, 4, -3],
-		[4, -2, 0],
-		[-3, 1, 1]
-	]
-
-	matrix.mprint(cholesky(M1))
-	matrix.mprint(cholesky(M2))
-	matrix.mprint(cholesky(M3))
+	if len(sys.argv) < 2:
+		print('usage:', sys.argv[0], '<input matrix filepath>')
+		exit(1)
+	m = matrix.mread(sys.argv[1])
+	L = cholesky(m)
+	matrix.mprint(L)
