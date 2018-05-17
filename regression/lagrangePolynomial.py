8df99dab1d9ac90d97eb17ffb14dea1b628e08fb
felsiq <felipe.siqueira@usp.br>
2018-05-17 13:45:39 -0300

    Added a Lagrange Polynomial implementation.

---
+++ regression/lagrangePolynomial.py
@@ -0,0 +1,35 @@
+from rpy2 import robjects as ro
+import sys
+import re
+
+def calcLk(x, k):
+    lk = ''
+    for j in range(len(x)):
+        if j != k:
+            lk += '(x - ' + str(x[j]) + ')/(' + (str(x[k] - x[j])) + ')*'
+    return lk[:-1]
+
+def lagrangeInterpol(x, y):
+    pol = ''
+    for k in range(len(x)):
+        pol += calcLk(x, k) + '*' + str(y[k]) + '+'
+    return pol[:-1]
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('usage:', sys.argv[0], '<x values separated by spaces> <y values separated by spaces> [(optional) lagrange polynomial input]')
+        exit(1)
+
+    x = list(map(float, sys.argv[1].split(' ')))
+    y = list(map(float, sys.argv[2].split(' ')))
+
+    if len(x) != len(y):
+        print('Lengths of x and y values must be the same.')
+        exit(2)
+
+    p = lagrangeInterpol(x, y)
+
+    print('Lagrange polynomial:', p)
+    if len(sys.argv) >= 4:
+        res = ro.r(re.sub(r'\bx\b', sys.argv[3], p))[0]
+        print('\nResult:', res)
