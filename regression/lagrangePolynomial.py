26c09735e9f55a6929a45ed6ce8d734af683a763
FelSiq <felipe.siqueira@usp.br>
2018-05-27 12:33:06 -0300

    Added plotting on Lagrange Pol technique.

--- regression/lagrangePolynomial.py
+++ regression/lagrangePolynomial.py
@@ -1,4 +1,6 @@
 from rpy2 import robjects as ro
+import matplotlib.pyplot as plt
+import numpy as np
 import sys
 import re
 
@@ -15,9 +17,25 @@ def lagrangeInterpol(x, y):
         pol += calcLk(x, k) + '*' + str(y[k]) + '+'
     return pol[:-1]
 
+def plot(pol, xlim=(-10.0, 10.0), num=1000, points=(None, None)):
+    x=[]
+    y=[]
+    inc=(xlim[1]-xlim[0])/num
+    for i in range(num):
+        val=i*inc+xlim[0]
+        x.append(val)
+        y.append(float(ro.r(re.sub(r'\bx\b', str(val), pol))[0]))
+
+    plt.plot(x, y)
+    if points is not None and points[0] is not None and points[1] is not None:
+        plt.plot(points[0], points[1], 'o')
+    plt.show()
+
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print('usage:', sys.argv[0], '<x values separated by spaces> <y values separated by spaces> [(optional) lagrange polynomial input]')
+        print('usage:', sys.argv[0], '<x values separated by',
+		'spaces> <y values separated by spaces>',
+		'[(optional) lagrange polynomial input]')
         exit(1)
 
     x = list(map(float, sys.argv[1].split(' ')))
@@ -30,6 +48,12 @@ if __name__ == '__main__':
     p = lagrangeInterpol(x, y)
 
     print('Lagrange polynomial:', p)
+
     if len(sys.argv) >= 4:
         res = ro.r(re.sub(r'\bx\b', sys.argv[3], p))[0]
         print('\nResult:', res)
+
+    # Result plot
+    intInc=(max(x)-min(x))*0.15
+    plot(p, xlim=(min(x)-intInc, max(x)+intInc), points=(x, y))
+
