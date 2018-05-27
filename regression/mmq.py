dfb5bb3489ed7229bb32a1198f65baa74b2487c1
FelSiq <felipe.siqueira@usp.br>
2018-05-27 12:32:55 -0300

    Added plotting on MMQ technique.

--- regression/mmq.py
+++ regression/mmq.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from rpy2 import robjects as ro
 import numpy as np
 import sys
@@ -37,6 +38,20 @@ def mmq(x, y, f):
 		approxFun += '(' + str(coeffs[i]) + ')*' + f[i] + ' + '
 	return approxFun[:-2]
 
+def plot(fun, xlim=(-10.0, 10.0), num=1000, points=(None, None)):
+    x=[]
+    y=[]
+    inc=(xlim[1]-xlim[0])/num
+    for i in range(num):
+        val=i*inc+xlim[0]
+        x.append(val)
+        y.append(float(ro.r(re.sub(r'\bx\b', str(val), fun))[0]))
+
+    plt.plot(x, y)
+    if points is not None and points[0] is not None and points[1] is not None:
+        plt.plot(points[0], points[1], 'o')
+    plt.show()
+
 if __name__ == '__main__':
 	if len(sys.argv) < 4:
 		print('usage:', sys.argv[0], 
@@ -60,3 +75,7 @@ if __name__ == '__main__':
 		for val in vlist:
 			print('f(' + str(val) + ')=', 
 				ro.r(re.sub(r'\bx\b', str(val), approxFunc))[0])
+
+	# Result plot
+	intInc=(max(x)-min(x))*0.15
+	plot(approxFunc, xlim=(min(x)-intInc, max(x)+intInc), points=(x, y))	
